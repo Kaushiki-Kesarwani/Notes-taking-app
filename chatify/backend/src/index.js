6c6962d1d6c0454a3aed2357785009dcdb175687
mahak <kaushikikes@gmail.com>
2026-05-28 20:57:40 +0530

    jwt authentication

--- chatify/backend/src/index.js
+++ chatify/backend/src/index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import {conn} from './config/db.js'
+import cookieParser from 'cookie-parser'
 import authroutes from './routes/authRoute.js'
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use('/api/auth',authroutes);
     
 const port = process.env.PORT || 5001
