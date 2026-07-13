5aba8a2b652bf94f23ba86222a70335b76cef813
mahak <kaushikikes@gmail.com>
2026-07-13 13:32:13 +0530

    verifying token in middleware and updated notesModel file

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import ratelimiter from './middleware/rateLimiter.js'
 import authRoutes from "./routes/auth.route.js"
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(cors(
   }
 ));
 app.use(express.json());
+app.use(cookieParser());
 app.use(ratelimiter); 
 
 app.use("/api/auth",authRoutes);
