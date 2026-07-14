0cbc25c80ab7bfb438fc9d78963bde8230e132dc
mahak <kaushikikes@gmail.com>
2026-07-14 12:53:37 +0530

    add authorization

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import { getNotes,createNotes ,updateNotes,deleteNotes,getSpecificNote} from '../controller/notesController.js'
+import { getNotes,createNotes ,updateNotes,deleteNotes} from '../controller/notesController.js'
+import {protect} from "../middleware/auth.middleware.js"
 
 const router = express.Router();
 
-router.get('/' , getNotes);
-router.get('/:id',getSpecificNote)
-router.put('/:id',updateNotes);
-router.post('/add',createNotes);
-router.delete('/:id',deleteNotes);
+router.get('/',protect, getNotes);
+router.put('/:id',protect,updateNotes);
+router.post('/add',protect,createNotes);
+router.delete('/:id',protect,deleteNotes);
 
 
 export default router;
