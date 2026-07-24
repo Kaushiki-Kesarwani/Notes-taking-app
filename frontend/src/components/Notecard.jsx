ad7ab4e6d533b70e78232fa435d92192e300f7a5
mahak <kaushikikes@gmail.com>
2026-07-24 14:06:50 +0530

    add pin notes feature

--- frontend/src/components/Notecard.jsx
+++ frontend/src/components/Notecard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
-import { PenSquareIcon, Trash2Icon } from "lucide-react";
+import { PenSquareIcon, Trash2Icon,PinIcon } from "lucide-react";
 import { formatDate } from "../lib/utils";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
@@ -28,16 +28,38 @@ const Notecard = ({ note, setNotes }) => {
   Important: "badge-error",
 };
 
+const handleTogglePin = async (e) =>{
+  e.stopPropagation();
+  e.preventDefault();
+
+  const previousPinned = note.isPinned;
+
+  setNotes((prevNotes)=>prevNotes.map((n) => n._id === note._id?{ ...n, isPinned: !n.isPinned }
+        : n));
+
+        try{
+          await api.patch(`/notes/${note._id}/pin`);
+        }catch(err){
+           console.log(error);
+
+    // Rollback UI
+    setNotes((prevNotes) =>
+      prevNotes.map((n) =>
+        n._id === note._id
+          ? { ...n, isPinned: previousPinned }
+          : n
+      )
+    );
+
+    toast.error("Failed to update pin.");
+        }
+}
+
   return (
     <Link
       to={`/notes/${note._id}`}
       className="block bg-base-100 p-5 rounded-2xl border border-base-300 shadow-[0_10px_30px_rgba(0,0,0,0.12)] hover:-translate-y-1 hover:shadow-[0_18px_45px_rgba(0,0,0,0.22)] transition-all duration-300"
     >
-
-
-
-
-
 
       <div className="card-body p-0">
         <h2 className="card-title text-base-content mb-2">{note.title}</h2>
@@ -79,17 +101,34 @@ const Notecard = ({ note, setNotes }) => {
             {formatDate(new Date(note.createdAt))}
           </span>
 
-          <div className="flex items-center gap-1">
-            <PenSquareIcon className="size-4 text-primary" />
+         <div className="flex items-center gap-1">
 
-            <button
-              onClick={handleDelete}
-              className="flex items-center justify-center w-9 h-9 rounded-full text-error hover:bg-error/15 hover:text-error transition-all duration-200 hover:scale-110 active:scale-95"
-              title="Delete Note"
-            >
-              <Trash2Icon className="size-4" />
-            </button>
-          </div>
+  <button
+    onClick={handleTogglePin}
+    className={`flex items-center justify-center w-9 h-9 rounded-full transition-all duration-200 hover:scale-110 ${
+      note.isPinned
+        ? "text-warning bg-warning/20"
+        : "text-base-content/50 hover:bg-base-300"
+    }`}
+    title={note.isPinned ? "Unpin Note" : "Pin Note"}
+  >
+    <PinIcon
+      className="size-4"
+      fill={note.isPinned ? "currentColor" : "none"}
+    />
+  </button>
+
+  <PenSquareIcon className="size-4 text-primary" />
+
+  <button
+    onClick={handleDelete}
+    className="flex items-center justify-center w-9 h-9 rounded-full text-error hover:bg-error/15 hover:text-error transition-all duration-200 hover:scale-110 active:scale-95"
+    title="Delete Note"
+  >
+    <Trash2Icon className="size-4" />
+  </button>
+
+</div>
         </div>
       </div>
     </Link>
